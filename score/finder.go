fe1173d9838cbf40d7c0db65663f08afd7e797c6
naronA <otherthan.new@gmail.com>
2019-07-15 14:27:45 +0900

    highlight修正

--- score/finder.go
+++ score/finder.go
@@ -1,8 +1,28 @@
 package score
 
 import (
+	"sort"
 	"strings"
 )
+
+type Range struct {
+	Start int
+	End   int
+}
+
+type Ranges []*Range
+
+func (r Ranges) Len() int {
+	return len(r)
+}
+
+func (r Ranges) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
+}
+
+func (r Ranges) Less(i, j int) bool {
+	return r[i].Start < r[j].Start
+}
 
 type Finder struct {
 	Source string
@@ -17,38 +37,61 @@ func (f Finder) Score() int {
 	return sc
 }
 
-func (f Finder) String() string {
-	hBegin := "\x1b[38;5;198m"
-	hEnd := "\x1b[0m"
-	highligh := []rune(f.Source)
-	for _, input := range f.Inputs {
-		point := strings.Index(string(highligh), input)
-		head := highligh[:point]
-		term := highligh[point : point+len(input)]
-		tail := highligh[point+len(input):]
+func overlap(start, end int, m Range) bool {
+	if m.Start < start && start < m.End && end >= m.End {
+		return true
+	}
+	return false
+}
 
-		highligh = make([]rune, 0, len(highligh)+2)
+func (f Finder) Matches() Ranges {
+	matches := Ranges{}
+	for _, input := range f.Inputs {
+		start := strings.Index(f.Source, input)
+		end := start + len(input)
+		isOverlap := false
+		for _, m := range matches {
+			if m.Start <= start && start <= m.End && end > m.End {
+				isOverlap = true
+				m.End = end
+				break
+			} else if start < m.Start && m.Start <= end && end <= m.End {
+				isOverlap = true
+				m.Start = start
+				break
+			} else if m.Start <= start && start <= m.End && m.Start <= end && end <= m.End {
+				isOverlap = true
+				break
+			}
+		}
+		if isOverlap == false {
+			m := &Range{Start: start, End: end}
+			matches = append(matches, m)
+		}
+		sort.Sort(matches)
+	}
+	return matches
+}
+
+func (f Finder) String() string {
+	hBegin := []rune("\x1b[38;5;198m")
+	hEnd := []rune("\x1b[0m")
+	highligh := []rune(f.Source)
+	for i, m := range f.Matches() {
+		gap := i * (len(hBegin) + len(hEnd))
+		head := highligh[:m.Start+gap]
+		term := highligh[m.Start+gap : m.End+gap]
+		tail := highligh[m.End+gap:]
+
+		highligh = make([]rune, 0, len(highligh)+(len(hBegin)+len(hEnd)))
 		highligh = append(highligh, head...)
-		highligh = append(highligh, '\t')
+		highligh = append(highligh, hBegin...)
 		highligh = append(highligh, term...)
-		highligh = append(highligh, '\v')
+		highligh = append(highligh, hEnd...)
 		highligh = append(highligh, tail...)
 	}
 
-	result := string(highligh)
-	for strings.Contains(result, "\t") || strings.Contains(result, "\v") {
-		result = strings.Replace(result, "\t", hBegin, 1)
-
-		// タグが入れ子になっていたら入れ子の部分を削除する
-		// \t ab \t cA \v BC \v  => \t ab cA BC \v
-		for strings.Contains(result, "\t") && strings.Index(result, "\t") < strings.Index(result, "\v") {
-			result = strings.Replace(result, "\t", "", 1)
-			result = strings.Replace(result, "\v", "", 1)
-		}
-
-		result = strings.Replace(result, "\v", hEnd, 1)
-	}
-	return result
+	return string(highligh)
 }
 
 type Finders []Finder
