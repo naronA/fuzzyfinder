57053f0a20e900980de0a69033f25ca7012e0534
naronA <otherthan.new@gmail.com>
2019-07-15 22:10:14 +0900

    test修正

--- score/util_test.go
+++ score/util_test.go
@@ -7,16 +7,16 @@ import (
 
 func TestIndicesAll(t *testing.T) {
 	ids := IndicesAll("abcdddabcdddabc", "abc")
-	fmt.Println(ids)
 	if ids[0] != 0 || ids[1] != 6 || ids[2] != 12 || len(ids) > 3 {
+		fmt.Println(ids)
 		t.Fail()
 	}
 }
 
 func TestIndicesAll2(t *testing.T) {
 	ids := IndicesAll("score/score.go", "or")
-	fmt.Println(ids)
 	if ids[0] != 2 || ids[1] != 8 || len(ids) > 2 {
+		fmt.Println(ids)
 		t.Fail()
 	}
 
